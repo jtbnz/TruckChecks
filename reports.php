1b6a7f595885dec443300a8c6ea3d3e703adc20b
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-19 08:46:22 +1200

    more report cleanup

--- reports.php
+++ reports.php
@@ -89,7 +89,7 @@ if (isset($_POST['export_csv'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Locker Check Reports</title>
-    <link rel="stylesheet" href="styles/reports.css"> <!-- Link to your CSS -->
+    <link rel="stylesheet" href="styles/reports.css?id=V5"> <!-- Link to your CSS -->
 </head>
 <body>
 
