3d4b848969c986d8544ec75e32b2d01a4f914b5d
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-19 09:07:54 +1200

    fix clean tables

--- demo_clean_tables.php
+++ demo_clean_tables.php
@@ -1,6 +1,7 @@
 <?php
-// Start the session
-session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Include the database connection
 include 'db.php';
