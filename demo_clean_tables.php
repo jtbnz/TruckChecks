1733d489fde96eee52fcb3ce1321296e98311ceb
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-19 09:40:08 +1200

    add in header for clean pages

--- demo_clean_tables.php
+++ demo_clean_tables.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL);
 // Include the database connection
 include 'db.php';
 $db = get_db_connection();
+include 'templates/header.php';
 
 // Check if the session variable 'is_demo' is set and true
 $showButton = isset($_SESSION['is_demo']) && $_SESSION['is_demo'] === true;
