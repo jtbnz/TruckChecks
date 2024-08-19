b03fe034cf4c792a59fe680a17727231d4027fba
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-19 16:31:34 +1200

    postion badge

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Truck Checklist</title>
-    <link rel="stylesheet" href="styles/styles.css?id=V8">
+    <link rel="stylesheet" href="styles/styles.css?id=V19">
 </head>
 <?$is_demo = isset($_SESSION['is_demo']) && $_SESSION['is_demo'] === true; ?>
 <body class="<?php echo $is_demo ? 'demo-mode' : ''; ?>">
