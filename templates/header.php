fba70fa810f3a3607fd30be738a4c8cfe18b9dd8
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-20 09:08:41 +1200

    pagespeed recommendations

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Truck Checklist</title>
     <link rel="stylesheet" href="styles/styles.css?id=V24">
 </head>
