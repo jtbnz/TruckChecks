00834014e73383b3f7772893a549733b7a556513
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-21 08:44:34 +1200

    try and clear the session

--- logout.php
+++ logout.php
@@ -1,14 +1,23 @@
 <?php
-// Start the session
+
 include 'header.php';
 
-// Remove the logged-in cookie
-setcookie('logged_in', '', time() - 3600, "/"); // Expire the cookie
-$_SESSION['version'] = null;
+// Unset all of the session variables
 $_SESSION = array();
+
+// delete the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// destroy the session
 session_destroy();
 
-// Redirect to login page
-header('Location: login.php');
+
+header("Location: login.php");
 exit;
 ?>
