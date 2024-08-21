a33df8548d70b178413e3d54e3e60baad9a76452
Jon <57672422+jtbnz@users.noreply.github.com>
2024-08-21 20:50:01 +1200

    try including styles

--- quiz/quiz.php
+++ quiz/quiz.php
@@ -99,7 +99,7 @@ if ($quiz === null) {
 <head>
     <meta charset="UTF-8">
     <title>Truck Item Quiz</title>
-    
+    <link rel="stylesheet" href="../styles/styles.css?id=<?php  echo $version;  ?> ">   
     <style>
 
 .quiz-container {
