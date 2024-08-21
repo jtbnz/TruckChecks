2fe471b29c2d5f89e9122f8070a7ff82c56bd51b
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-22 09:49:01 +1200

    tidy up quiz button

--- check_locker_items.php
+++ check_locker_items.php
@@ -272,7 +272,14 @@ if ($selected_truck_id) {
     <p>Please select a truck to view its lockers and items.</p>
 <?php endif; ?>
 
-<?php include 'templates/footer.php'; ?>
-<a href="quiz.html" class="button touch-button">Quiz</a> 
+<footer>
+    <? $version = $_SESSION['version']; ?>
+    <p><a href="index.php" class="button touch-button">Return to Home</a></p>
+    <p><a href="quiz.html" class="button touch-button">Locker Quiz</a> </p>
+    <p id="last-refreshed" style="margin-top: 10px;"></p> 
+    <div class="version-number">
+        Version: <?php echo htmlspecialchars($version); ?>
+    </div>   
+</footer>
 </body>
 </html>
