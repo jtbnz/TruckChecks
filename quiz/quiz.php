aceee281a49a358c43cffc79e8bfc1337aec241a
Jon <57672422+jtbnz@users.noreply.github.com>
2024-08-21 20:05:54 +1200

    fix styles

--- quiz/quiz.php
+++ quiz/quiz.php
@@ -12,6 +12,18 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+// Check if the version session variable is not set
+if (!isset($_SESSION['version'])) {
+    // Get the latest Git tag version
+    $version = trim(exec('git describe --tags $(git rev-list --tags --max-count=1)'));
+
+    // Set the session variable
+    $_SESSION['version'] = $version;
+} else {
+    // Use the already set session variable
+    $version = $_SESSION['version'];
+}
+
 // Check if the timestamp is already set
 if (!isset($_SESSION['quiz_start_time'])) {
     $_SESSION['quiz_start_time'] = time();
@@ -85,7 +97,7 @@ if ($quiz === null) {
 <head>
     <meta charset="UTF-8">
     <title>Truck Item Quiz</title>
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="../styles/styles.css?id=<?php  echo $version;  ?> ">
     <style>
         .quiz-container {
             text-align: center;
