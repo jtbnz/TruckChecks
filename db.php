51d240960eda175df874228f874f7637485c6aa5
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-22 14:04:41 +1200

    remove echo

--- db.php
+++ db.php
@@ -2,9 +2,9 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-ini_set('display_errors', 1);
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 
 
 include 'config.php';
@@ -14,8 +14,6 @@ function get_db_connection() {
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=" .db_host . ";dbname=" . db_name . ";charset=$charset";
-
-    echo $dsn;
 
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,  // Throw exceptions for errors
