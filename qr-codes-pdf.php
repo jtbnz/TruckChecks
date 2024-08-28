029159a3eb93583e12fcbe934658723beb485b47
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-29 09:43:53 +1200

    fix columns

--- qr-codes-pdf.php
+++ qr-codes-pdf.php
@@ -47,8 +47,10 @@ $writer = new PngWriter();
 
 
 foreach ($lockers as $index => $locker) {
-    if ($index != 0 && $index % ($labelsPerRow * $labelsPerColumn) == 0) {
-        $pdf->AddPage();
+    if ($index % ($labelsPerRow * $labelsPerColumn) == 0) {
+        if ($index != 0) {
+            $pdf->AddPage();
+        }
     }
 
     $row = floor($index / $labelsPerRow) % $labelsPerColumn;
