1e314eb502fb856ab6200db90488d31cf485b03e
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-29 11:34:44 +1200

    fix missing char

--- qr-codes-pdf.php
+++ qr-codes-pdf.php
@@ -71,7 +71,7 @@ foreach ($lockers as $index => $locker) {
 
     //$pdf->SetFont('helvetica', '', 6);
     $pdf->SetFont('freemono', '', 6);
-    $pdf->Text($x + 6, $y - 3, $locker['truck_name'] . ' ' . $locker['locker_name'] . chr(0x261F));
+    $pdf->Text($x + 6, $y - 3, $locker['truck_name'] . ' ' . $locker['locker_name'] . '    VVVVVV');
     $pdf->Image('@' . $writer->write($qrCode)->getString(), $x, $y, $qrCodeSize, $qrCodeSize, 'PNG');
     //echo "Index: $index Row: $row, Col: $col, x: $x , y: $y " . $locker['truck_name'] . ' ' . $locker['locker_name'] . "<br>";   
    
