b19bb495010c6fb33f790118b2bbd25df31f3680
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-08-31 15:02:08 +1200

    debug config

--- admin.php
+++ admin.php
@@ -1,6 +1,17 @@
 <?php
 
+
 include('config.php');
+if (DEBUG) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    echo "Debug mode is on";
+} else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(0);
+}
 include ('db.php');
 
 include 'templates/header.php';
--- db.php
+++ db.php
@@ -2,12 +2,18 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-/* ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); */
 
-
-include 'config.php';
+include('config.php');
+if (DEBUG) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    echo "Debug mode is on";
+} else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(0);
+}
 
 function get_db_connection() {
 
