1101cf5c7391168bef9589e6b7ebcb509a927b40
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-09-03 09:40:16 +1200

    read the CB mode before setting

--- settings.php
+++ settings.php
@@ -3,6 +3,9 @@
 include('config.php');
 include 'templates/header.php';
 
+// Read the cookie value
+$colorBlindMode = isset($_COOKIE['color_blind_mode']) ? $_COOKIE['color_blind_mode'] : false;
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $colorBlindMode = isset($_POST['color_blind_mode']) ? true : false;
     setcookie('color_blind_mode', $colorBlindMode, time() + (86400 * 180), '/'); // Set the cookie for 180 days
