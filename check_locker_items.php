781c7a7c8b5b4e3ed828ee00db23a51f59da8ff8
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-11-05 09:54:28 +1300

    highlight the locker has been checked today

--- check_locker_items.php
+++ check_locker_items.php
@@ -168,7 +168,7 @@ if ($selected_truck_id) {
 
 
             if ($days_since_last_check == 0) {
-                $days_since_last_check_text = "Locker has been checked today by " . htmlspecialchars($last_check['checked_by']);
+                $days_since_last_check_text = "<span style='color: green;'>Locker has been checked today by " . htmlspecialchars($last_check['checked_by'] . "</span>");
                 $last_check_text = "";
             } else {
                 $days_since_last_check_text = "Days since last check: " . $days_since_last_check  ;
