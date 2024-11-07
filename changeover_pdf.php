36899dcc4537ae15b7b5a58474493a0bff18fb12
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-11-07 13:19:52 +1300

    fix: adjust HTML table header structure in changeover_pdf.php for improved readability

--- changeover_pdf.php
+++ changeover_pdf.php
@@ -111,9 +111,9 @@ $html = '';
                 }
 
                 $html .=  '<tr style="background-color: #A9A9A9">' ;
-                $html .=  "<th><strong>" . htmlspecialchars($row['locker_name']) . "</strong></th>";
-                $html .= "<th>Relief</th><th>" .$truck['name'] . "</th><th><strong>" . htmlspecialchars($row['locker_name']) . "</strong></th><th>Relief</th><th>" .$truck['name'] . "</th><TR>";
-                
+                 $html .= "<th>Relief</th><th>" .$truck['name'] . "</th><th><strong>" . htmlspecialchars($row['locker_name']) . "</strong></th><th>Relief</th><th>" .$truck['name'] . "</th><TR>";
+                 $html .=  "<th><strong>" . htmlspecialchars($row['locker_name']) . "</strong></th>";
+               
                 
                 if ($locker_total % 2 == 0) {
                     $cellbgcolour = "#ffffff";
