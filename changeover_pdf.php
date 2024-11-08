fc3386f23a748d1c2338dc18954902325e64174c
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-11-08 14:09:13 +1300

    fix: comment out PDF write and page addition logic to prevent premature output

--- changeover_pdf.php
+++ changeover_pdf.php
@@ -168,8 +168,8 @@ $html = '';
  
             if ($rowcount > 26) {
                 $html .=  "</tbody></table>";
-                $pdf->writeHTML($html, true, false, false, false, '');
-                $pdf->AddPage();
+                //$pdf->writeHTML($html, true, false, false, false, '');
+                //$pdf->AddPage();
                 $html = '';
                 $html .=  '<table border="1" cellpadding="5" cellspacing="0" style="width: 100%;">';
                 $html .=  "<tbody>";
