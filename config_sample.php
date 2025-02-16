7998f8e109a778a974ae06010d6c998bc64b3627
jtbnz <57672422+jtbnz@users.noreply.github.com>
2025-02-17 12:06:45 +1300

    feat: implement email sending functionality using PHPMailer

--- config_sample.php
+++ config_sample.php
@@ -14,6 +14,12 @@ optional add an .htaccess to further restrict access
  if (!defined('DB_USER'))   define('DB_USER'  , 'your_username');
  if (!defined('DB_PASS'))   define('DB_PASS'  , 'your_password');
  if (!defined('PASSWORD'))  define('PASSWORD' , 'YourSecurePassword'); //Used for access to the Admin pages
+
+ if (!defined('EMAIL_HOST'))   define("EMAIL_HOST" ,"smtp host");
+ if (!defined('EMAIL_USER')) define('EMAIL_USER', 'email addess');
+ if (!defined('EMAIL_PASS')) define('EMAIL_PASS', 'email password');
+ if (!defined('EMAIL_PORT'))   define('EMAIL_PORT' , 'SMTP outgoing port');
+
  if (!defined('TZ_OFFSET')) define('TZ_OFFSET','+12:00'); //If you need to change timezones
  if (!defined('IS_DEMO'))   define('IS_DEMO'  , false);
  if (!defined('REFRESH'))   define('REFRESH'  , 30000); // 30000 = 30 seconds this is how often the main page will auto refresh
