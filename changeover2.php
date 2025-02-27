65334890875deb1947a6d01b7b1201fdef40d3fd
jtbnz <57672422+jtbnz@users.noreply.github.com>
2025-02-27 15:32:33 +1300

    fix: remove fixed width from table cell in changeover2.php for better responsiveness

--- changeover2.php
+++ changeover2.php
@@ -197,7 +197,7 @@
         
             $is_relief = isset($current_items[$row['item_name']]) ? $current_items[$row['item_name']]['is_relief'] : false;
             
-            echo "<td style='width:80%;background-color: {$cellbgcolour}'>";
+            echo "<td style='background-color: {$cellbgcolour}'>";
             echo htmlspecialchars($row['item_name']);
             echo "<label class='toggle-switch'>";
             echo "</td><td style='width:20%;><input type='checkbox' name='items[{$row['item_name']}][state]' " . ($is_relief ? '' : 'checked') . ">";
